2aa33428db193a7446f059de97eb6312f215a4b8
Mauro017 <carlosmauricioarevalozapata13@gmail.com>
2025-05-02 00:00:38 -0500

    Proyecto Django

---
+++ core/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render,HttpResponse
+
+html_base = """
+<h1>Gangazos</h1>
+<ul>
+    <li><a href="/">Inicio</a></li>
+    <li><a href="/about">Sobre los Gangazos</a></li>
+    <li><a href="/portafolio">Categorias en oferta</a></li>
+    <li><a href="/contact">Contactenos</a></li>
+</ul>
+"""
+
+# Create your views here.
+def home(request):
+    return render(request,"core/home.html")
+
+def about(request):
+    return render(request,"core/about.html")
+
+def portafolio(request):
+    return render(request,"core/portafolio.html")
+
+def contact(request):
+    return render(request,"core/contact.html")
